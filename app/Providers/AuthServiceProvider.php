498c0bde2d1fef986afc9f2753bc4a837eed1682
Josh L <imtheface111@gmail.com>
2022-09-10 22:38:33 +0100

    initial commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -17,21 +17,25 @@ class AuthServiceProvider extends ServiceProvider
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->registerPolicies();
+        $this->registerUserAccessToGates();
+    }
 
-        foreach (Permission::pluck('name') as $permission) {
-            Gate::define($permission, function ($user) use ($permission) {
-                return $user->roles()->whereHas('permissions', function ($q) use ($permission) {
-                    $q->where('name', $permission);
-                })->count() > 0;
-            });
+    protected function registerUserAccessToGates()
+    {
+        try {
+            foreach (Permission::pluck('name') as $permission) {
+                Gate::define($permission, function ($user) use ($permission) {
+                    return $user->roles()->whereHas('permissions', function ($q) use ($permission) {
+                        $q->where('name', $permission);
+                    })->count() > 0;
+                });
+            }
+        } catch (\Exception $e) {
+            info('registerUserAccessToGates: Database not found or not yet migrated. Ignoring user permissions while booting app.');
         }
     }
+
 }
