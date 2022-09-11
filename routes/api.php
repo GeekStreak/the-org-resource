c3fb1a0437ccb52b7fe5ff6e3f532a9ef8e89a4b
Josh L <imtheface111@gmail.com>
2022-09-11 01:54:16 +0100

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::get('/user', function (Request $request) {
+        console.log("middleware");
         return $request->user();
     });
 });
