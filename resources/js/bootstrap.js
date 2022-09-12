988aff07cff62ce3ad38267755d920230186a1ab
Josh L <imtheface111@gmail.com>
2022-09-12 22:44:10 +0100

    initial commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,19 +11,19 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.axios.defaults.withCredentials = true
-window.axios.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response?.status === 401 || error.response?.status === 419) {
-            if (JSON.parse(localStorage.getItem('loggedIn'))) {
-                localStorage.setItem('loggedIn', false)
-                location.assign('/login')
-            }
-        }
+//window.axios.interceptors.response.use(
+    //response => response,
+    //error => {
+        //if (error.response?.status === 401 || error.response?.status === 419) {
+            //if (JSON.parse(localStorage.getItem('loggedIn'))) {
+                //localStorage.setItem('loggedIn', false)
+                //location.assign('/login')
+            //}
+        //}
 
-        return Promise.reject(error)
-    }
-)
+        //return Promise.reject(error)
+    //}
+//)
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
