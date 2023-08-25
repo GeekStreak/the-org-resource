2026b6b7bb18777f27bc1daab1d5f940e6c6f133
geekstreak10 <bombgoboom10@gmail.com>
2023-08-25 20:33:27 +0100

    initial commit

--- resources/js/components/Posts/Resources.vue
+++ resources/js/components/Posts/Resources.vue
@@ -1,5 +1,11 @@
 <template>
 <body>
+        <div class="flex justify-center items-center">
+            <div>
+                <h2 class="m-0 text-sm text-center"><b>Looking For Inspiration?</b></h2>
+                <button class="text-center bg-indigo-500 hover:bg-indigo-700 text-white text-sm font-bold py-2 px-4 m-2 border border-blue-700 rounded"><router-link :to="{name: 'posts.RandomChallenge'}">Generate A Random Challenge</router-link></button><br><br>
+            </div>
+        </div>
         <tbody class="bg-white divide-y divide-gray-200 divide-solid">
             <tr v-for="resource in resources.data">
                 <h2 class="px-6 py-2 whitespace-no-wrap text-sm leading-5 text-gray-900"><b>{{ resource.title }}</b></h2>
